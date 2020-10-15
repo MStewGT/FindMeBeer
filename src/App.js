efe114134a49dd4203bd6028994417dfdf4dd2f0
Matt S <mstewgt@gmail.com>
2020-10-15 13:22:10 -0500

    test change to length of return field from api

--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ function App() {
                 <div class="container">
                   City: {brewery.city}
                   <br></br>
-                  {brewery.website_url != null &&
+                  {brewery.website_url.length > 0 null &&
                     <a href={brewery.website_url}>Website</a>
                   }
                 </div>
