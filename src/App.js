e684db4ac3aeaddafcc104646ddb4435e3209303
momosetti <zinasetti@gmail.com>
2020-10-18 03:52:28 +0100

    Presenting an error message when nothing is returned from the API - fix #6

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
 import { useState, useCallback } from "react";
-
+import "rsuite/dist/styles/rsuite-default.css";
+import { Alert } from "rsuite";
 function App() {
-
-  const [locData, setLocData] = useState('Podunk')
+  const [locData, setLocData] = useState("Podunk");
   const [brewData, setBrewData] = useState([]);
 
   const onLocChange = useCallback((event) => {
@@ -11,66 +11,73 @@ function App() {
     setLocData(event.target.value);
   }, []);
 
-  const formSubmitted = useCallback((event) => {
-    event.preventDefault();
-    console.log('Form was submitted!');
-    console.log(locData);
-    getBreweries(locData);
-  }, [locData]);
+  const formSubmitted = useCallback(
+    (event) => {
+      event.preventDefault();
+      console.log("Form was submitted!");
+      console.log(locData);
+      getBreweries(locData);
+    },
+    [locData]
+  );
 
-  const clearOnInputClick = () =>{
+  const clearOnInputClick = () => {
     setLocData("");
   };
 
   function getBreweries(location) {
-    const transformedLocation = location.split(' ').join('_')
+    const transformedLocation = location.split(" ").join("_");
     fetchBrew();
     async function fetchBrew() {
-        const res = await fetch(
-            `https://api.openbrewerydb.org/breweries?by_city=${transformedLocation}`
-        );
-        const data = await res.json();
-        setBrewData(data);
-        console.log(data);
-    }
-  };
+      const res = await fetch(
+        `https://api.openbrewerydb.org/breweries?by_city=${transformedLocation}`
+      );
+      const data = await res.json();
+      setBrewData(data);
+      console.log(data);
 
-  if (brewData == null) return ( "Sorry we could not find any results for that location." );
+      if (data.length === 0) {
+        Alert.error("Sorry, no result for your location.", 5000);
+      }
+    }
+  }
+
+  // return ( "Sorry we could not find any results for that location." );
 
   return (
-      <div>
-        <div class="header">
-          <h1>Find Me Beer!</h1>
-        </div>
-          Returns a list of breweries in a given city.
-          <br></br>
-          <br></br>
-          <form onSubmit={formSubmitted}>
-            <label>Enter a city:</label>
-            <input
-              value={locData}
-              onChange={onLocChange}
-              onClick={clearOnInputClick}
-            />
-            <button>GO</button>
-          </form>
-          <div class="list">
-            {brewData.map((brewery) => (
-              <div key={brewery.id} class="card">
-                <h4>{brewery.name}</h4>
-                <div class="container">
-                  {brewery.street}
-                  <br></br>
-                  {brewery.city}, {brewery.state} {brewery.postal_code}
-                  <br></br>
-                  {brewery.website_url.length > 0 &&
-                    <a href={brewery.website_url}>Website</a>
-                  }
-                </div>
-              </div>
-            ))}
-          </div>
+    <div>
+      <div class="header">
+        <h1>Find Me Beer!</h1>
       </div>
+      Returns a list of breweries in a given city.
+      <br></br>
+      <br></br>
+      <form onSubmit={formSubmitted}>
+        <label>Enter a city:</label>
+        <input
+          value={locData}
+          onChange={onLocChange}
+          onClick={clearOnInputClick}
+        />
+        <button>GO</button>
+      </form>
+      <div class="list">
+        {brewData.map((brewery) => (
+          <div key={brewery.id} class="card">
+            <h4>{brewery.name}</h4>
+            <div class="container">
+              {brewery.street}
+              <br></br>
+              {brewery.city}, {brewery.state} {brewery.postal_code}
+              <br></br>
+              {brewery.website_url.length > 0 && (
+                <a href={brewery.website_url}>Website</a>
+              )}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
 
